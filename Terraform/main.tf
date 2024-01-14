b9a7841797801478c34159a9948f20f19b91c2de
Aleksander <arnachimm@gmail.com>
2024-01-15 00:34:30 +0100

    Up terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -31,23 +31,10 @@ resource "google_sql_database_instance" "mysql_instance" {
   }
 }
 
-resource "google_sql_database" "default" {
-  name     = "terraform-sql"
-  instance = google_sql_database_instance.mysql_instance.name
-}
-
-resource "google_sql_user" "users" {
-  name     = "terraform-sql-user"
-  instance = google_sql_database_instance.mysql_instance.name
-  password = "terraform-sql-password"
-}
-
-output "db_connection_string" {
-  sensitive = true
-  value     = "mysql://${google_sql_user.users.name}:${google_sql_user.users.password}@${google_sql_database_instance.mysql_instance.ip_address.0.ip_address}/${google_sql_database.default.name}"
-}
-
 resource "google_pubsub_topic" "tasks_topic" {
-  name = "terraform-tasks-topic"
+  name = "tasks"
 }
 
+resource "google_pubsub_topic" "messages_topic" {
+  name = "messages"
+}
