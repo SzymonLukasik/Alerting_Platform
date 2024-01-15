05a404dfb006687c2145707830b2f5aaa9a99865
SzymonLukasik <szymon123lukasik@gmail.com>
2024-01-16 00:51:38 +0100

    Insert dummy data for testing

--- AlertAck/alert.sql
+++ AlertAck/alert.sql
@@ -2,8 +2,8 @@ CREATE TABLE alerts (
     id                      INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     url                     VARCHAR(4096),
     alertStatus             ENUM(
-	'completed',
-	'timed-out',
+	'success',
+	'timeout',
 	'error'
 	  ),
     responseStatus          ENUM('responded', 'ignored'),
@@ -13,4 +13,20 @@ CREATE TABLE alerts (
     secondAlertTime         DATETIME,
     firstAlertResponseTime  DATETIME,
     secondAlertResponseTime DATETIME
+);
+
+CREATE INDEX firstLinkUUID ON alerts (firstLinkUUID);
+CREATE INDEX secondLinkUUID ON alerts (secondLinkUUID);
+
+-- Insert some dummy data
+INSERT INTO alerts (url, alertStatus, responseStatus, firstLinkUUID, secondLinkUUID, firstAlertTime, secondAlertTime, firstAlertResponseTime, secondAlertResponseTime) VALUES (
+    'http://www.google.com',
+    'timeout',
+    NULL,
+    UNHEX(REPLACE('75314b56-67ab-4f51-bb4a-03471f9e8efe', '-', '')),
+    NULL,
+    '2015-01-01 00:00:00',
+    NULL,
+    NULL,
+    NULL
 );
