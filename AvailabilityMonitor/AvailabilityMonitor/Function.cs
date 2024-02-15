2d8531266912c0daff08244bf96308ed7b4d1ff4
Aleksander <arnachimm@gmail.com>
2024-02-15 22:05:52 +0100

    Up

--- AvailabilityMonitor/AvailabilityMonitor/Function.cs
+++ AvailabilityMonitor/AvailabilityMonitor/Function.cs
@@ -73,7 +73,7 @@ public class Function : IHttpFunction
                                            inner join {ServicesTable} s
                                                      on c.service_id = s.id
                                   where c.timestamp <= now()
-                                    and c.timestamp >= date_sub(now(), interval (s.alerting_window_ms * 1000) second)
+                                    and c.timestamp >= date_sub(now(), interval (s.alerting_window_ms / 1000) second)
                                   group by c.service_id, s.expected_availability
                                   having count(if(c.callResult = 'success', 1, null)) / count(*) < s.expected_availability
                               """;
